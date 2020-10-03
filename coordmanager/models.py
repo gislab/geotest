df6fb96690b825377758ce89f9f6ce72ce2a5ff6
Maurizio Melani (melamala) <maurizio@forwalk.org>
2020-10-03 09:34:16 +0200

    add 'Coordinate' models to store x,y coordinates

--- coordmanager/models.py
+++ coordmanager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Coordinate(models.Model):
+    """
+    Stores coordinates points
+    """
+    x = models.FloatField(help_text=_("longitude"))
+    y = models.FloatField(help_text=_("latitude"))
+
+    class Meta:
+        verbose_name = _("coordinate")
+        verbose_name_plural = _("coordinates")
+
+    def __str__(self):
+        return 'coordinate(id=%s, x=%s, y=%s)' % (self.id, self.x, self.y)
